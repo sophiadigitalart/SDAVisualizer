813e2110142ab9f27c22cae3b3885db284aecdb0
brucelane <bruce@batchass.fr>
2018-10-08 11:04:38 +0200

    Cinder vs2017 Spout Receiver app with Cinder-SDA and Cinder-Spout blocks

---
+++ src/SDAVisualizerApp.cpp
@@ -0,0 +1,198 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+
+// Settings
+#include "SDASettings.h"
+// Session
+#include "SDASession.h"
+// Log
+#include "SDALog.h"
+// Spout
+#include "CiSpoutIn.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+using namespace SophiaDigitalArt;
+
+class SDAVisualizerApp : public App {
+
+public:
+	SDAVisualizerApp();
+	void mouseMove(MouseEvent event) override;
+	void mouseDown(MouseEvent event) override;
+	void mouseDrag(MouseEvent event) override;
+	void mouseUp(MouseEvent event) override;
+	void keyDown(KeyEvent event) override;
+	void keyUp(KeyEvent event) override;
+	void fileDrop(FileDropEvent event) override;
+	void update() override;
+	void draw() override;
+	void cleanup() override;
+	void setUIVisibility(bool visible);
+private:
+	// Settings
+	SDASettingsRef					mSDASettings;
+	// Session
+	SDASessionRef					mSDASession;
+	// Log
+	SDALogRef						mSDALog;
+	// Spout
+	SpoutIn	mSpoutIn;
+	// fbo
+	bool							mIsShutDown;
+	Anim<float>						mRenderWindowTimer;
+	void							positionRenderWindow();
+	bool							mFadeInDelay;
+
+};
+
+
+SDAVisualizerApp::SDAVisualizerApp()
+{
+	// Settings
+	mSDASettings = SDASettings::create();
+	// Session
+	mSDASession = SDASession::create(mSDASettings);
+	//mSDASettings->mCursorVisible = true;
+	setUIVisibility(mSDASettings->mCursorVisible);
+	mSDASession->getWindowsResolution();
+
+	mFadeInDelay = true;
+	// windows
+	mIsShutDown = false;
+	mRenderWindowTimer = 0.0f;
+	timeline().apply(&mRenderWindowTimer, 1.0f, 2.0f).finishFn([&] { positionRenderWindow(); });
+
+}
+void SDAVisualizerApp::positionRenderWindow() {
+	mSDASettings->mRenderPosXY = ivec2(mSDASettings->mRenderX, mSDASettings->mRenderY);//20141214 was 0
+	setWindowPos(mSDASettings->mRenderX, mSDASettings->mRenderY);
+	setWindowSize(mSDASettings->mRenderWidth, mSDASettings->mRenderHeight);
+}
+void SDAVisualizerApp::setUIVisibility(bool visible)
+{
+	if (visible)
+	{
+		showCursor();
+	}
+	else
+	{
+		hideCursor();
+	}
+}
+void SDAVisualizerApp::fileDrop(FileDropEvent event)
+{
+	
+}
+void SDAVisualizerApp::update()
+{
+	mSDASession->setFloatUniformValueByIndex(mSDASettings->IFPS, getAverageFps());
+	mSDASession->update();
+}
+void SDAVisualizerApp::cleanup()
+{
+	if (!mIsShutDown)
+	{
+		mIsShutDown = true;
+		CI_LOG_V("shutdown");
+		// save settings
+		mSDASettings->save();
+		mSDASession->save();
+		quit();
+	}
+}
+void SDAVisualizerApp::mouseMove(MouseEvent event)
+{
+	if (!mSDASession->handleMouseMove(event)) {
+		// let your application perform its mouseMove handling here
+	}
+}
+void SDAVisualizerApp::mouseDown(MouseEvent event)
+{
+	if (!mSDASession->handleMouseDown(event)) {
+		// let your application perform its mouseDown handling here
+		if (event.isRightDown()) { 
+			// Select a sender
+			// SpoutPanel.exe must be in the executable path
+			mSpoutIn.getSpoutReceiver().SelectSenderPanel(); // DirectX 11 by default
+		}
+	}
+}
+void SDAVisualizerApp::mouseDrag(MouseEvent event)
+{
+	if (!mSDASession->handleMouseDrag(event)) {
+		// let your application perform its mouseDrag handling here
+	}	
+}
+void SDAVisualizerApp::mouseUp(MouseEvent event)
+{
+	if (!mSDASession->handleMouseUp(event)) {
+		// let your application perform its mouseUp handling here
+	}
+}
+
+void SDAVisualizerApp::keyDown(KeyEvent event)
+{
+	if (!mSDASession->handleKeyDown(event)) {
+		switch (event.getCode()) {
+		case KeyEvent::KEY_ESCAPE:
+			// quit the application
+			quit();
+			break;
+		case KeyEvent::KEY_h:
+			// mouse cursor and ui visibility
+			mSDASettings->mCursorVisible = !mSDASettings->mCursorVisible;
+			setUIVisibility(mSDASettings->mCursorVisible);
+			break;
+		}
+	}
+}
+void SDAVisualizerApp::keyUp(KeyEvent event)
+{
+	if (!mSDASession->handleKeyUp(event)) {
+	}
+}
+
+void SDAVisualizerApp::draw()
+{
+	gl::clear(Color::black());
+	if (mFadeInDelay) {
+		mSDASettings->iAlpha = 0.0f;
+		if (getElapsedFrames() > mSDASession->getFadeInDelay()) {
+			mFadeInDelay = false;
+			timeline().apply(&mSDASettings->iAlpha, 0.0f, 1.0f, 1.5f, EaseInCubic());
+		}
+	}
+
+	//gl::setMatricesWindow(toPixels(getWindowSize()),false);
+	//gl::setMatricesWindow(mSDASettings->mRenderWidth, mSDASettings->mRenderHeight, false);
+	//gl::draw(mSDASession->getMixTexture(), getWindowBounds());
+
+	auto tex = mSpoutIn.receiveTexture();
+	if (tex) {
+		// Otherwise draw the texture and fill the screen
+		gl::draw(tex, getWindowBounds());
+
+		// Show the user what it is receiving
+		gl::ScopedBlendAlpha alpha;
+		gl::enableAlphaBlending();
+		gl::drawString("Receiving from: " + mSpoutIn.getSenderName(), vec2(toPixels(20), toPixels(20)), Color(1, 1, 1), Font("Verdana", toPixels(24)));
+		gl::drawString("fps: " + std::to_string((int)getAverageFps()), vec2(getWindowWidth() - toPixels(100), toPixels(20)), Color(1, 1, 1), Font("Verdana", toPixels(24)));
+		gl::drawString("RH click to select a sender", vec2(toPixels(20), getWindowHeight() - toPixels(40)), Color(1, 1, 1), Font("Verdana", toPixels(24)));
+	}
+	else {
+		gl::ScopedBlendAlpha alpha;
+		gl::enableAlphaBlending();
+		gl::drawString("No sender/texture detected", vec2(toPixels(20), toPixels(20)), Color(1, 1, 1), Font("Verdana", toPixels(24)));
+	}
+	getWindow()->setTitle(mSDASettings->sFps + " fps SDA");
+}
+
+void prepareSettings(App::Settings *settings)
+{
+	settings->setWindowSize(640, 480);
+}
+
+CINDER_APP(SDAVisualizerApp, RendererGl, prepareSettings)
